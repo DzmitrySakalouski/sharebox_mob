f4aeac33b264c8933356052fed0668587581cf67
Dzmitry <Sakalouski>
2019-12-05 16:30:42 +0300

    working

--- src/screens/login/loginScreen.js
+++ src/screens/login/loginScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View, StyleSheet, ImageBackground } from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
 export const LoginScreen = (props) => {
     return (
         <View  style={styles.container}>
             <ImageBackground source={{ uri: 'https://dvmzgq36yy8ja.cloudfront.net/wp-content/uploads/2018/07/Hellfest-Open-Air-Festival.jpg' }} style={styles.imageBack}/>
+            <Icon name="archive" size={30} color="black" />
         </View>
     );
 }
