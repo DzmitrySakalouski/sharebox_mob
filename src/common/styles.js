eaefae77c4e811b7ef2c21f268d88b026a43f54d
Dzmitry <Sakalouski>
2019-12-05 17:29:16 +0300

    add login

---
+++ src/common/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const common = StyleSheet.create({
+    fontRoboto: {
+        // fontFamily: 'Comfortaa-Regular' 
+    },
+    center: {
+        flex: 0,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
--- src/screens/login/loginScreen.js
+++ src/screens/login/loginScreen.js
@@ -1,23 +1,60 @@
-import React from 'react';
-import { View, StyleSheet, ImageBackground } from 'react-native';
-import Icon from 'react-native-vector-icons/EvilIcons';
-
-export const LoginScreen = (props) => {
-    return (
-        <View  style={styles.container}>
-            <ImageBackground source={{ uri: 'https://dvmzgq36yy8ja.cloudfront.net/wp-content/uploads/2018/07/Hellfest-Open-Air-Festival.jpg' }} style={styles.imageBack}/>
-            <Icon name="archive" size={30} color="black" />
-        </View>
-    );
-}
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, Alert } from 'react-native';
+import firebase from 'react-native-firebase';
+import { Text, Input, Button } from 'react-native-elements';
+import { common } from '../../common';
 
 const styles = StyleSheet.create({
     imageBack: {
         width: '100%',
-        height: '50%'
+        height: '30%',
+        flex: 0,
+        resizeMode: 'stretch'
     },
     container: {
         flex: 1,
         justifyContent: 'space-between'
+    },
+    containerForm: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    formGroup: {
+        width: '100%',
+        paddingHorizontal: 50,
+        marginBottom: 50
+    },
+    formItem: {
+        marginTop: 20,
+    },
+    title: {
+        height: 150
     }
 });
+
+export const LoginScreen = (props) => {
+    const [email, setEmail] = useState('dmitry89sokol@gmail.com');
+    const [password, setPassword] = useState('08061989');
+
+    const logIn = () => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+            Alert('hello')
+        })
+    }
+
+    return (
+        <View  style={styles.container}>
+            <Image source={{ uri: 'https://dvmzgq36yy8ja.cloudfront.net/wp-content/uploads/2018/07/Hellfest-Open-Air-Festival.jpg' }} style={styles.imageBack}/>
+            <View style={styles.containerForm}>
+                <View style={[common.center, styles.title]}>
+                    <Text style={common.fontRoboto} h3>Sign In</Text>
+                </View>                
+                <View style={styles.formGroup}>
+                    <Input containerStyle={styles.formItem} value={email} placeholder="Email" onChangeText={text => setEmail(text)}/>
+                    <Input containerStyle={styles.formItem} value={password} secureTextEntry={true} onChangeText={text => setPassword(text)} placeholder="Password"/>
+                </View>
+                <Button onPress={logIn} title="Sign In" />
+            </View>
+        </View>
+    );
+}
