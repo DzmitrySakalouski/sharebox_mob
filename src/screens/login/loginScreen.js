9505ec0e64f3da14c39ed0f65e80899c8cdd69b0
Dzmitry <Sakalouski>
2019-12-05 16:03:55 +0300

    working app

--- src/screens/login/loginScreen.js
+++ src/screens/login/loginScreen.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { View, StyleSheet, ImageBackground } from 'react-native';
-import { Text } from 'react-native-elements';
 
 export const LoginScreen = (props) => {
     return (
         <View  style={styles.container}>
             <ImageBackground source={{ uri: 'https://dvmzgq36yy8ja.cloudfront.net/wp-content/uploads/2018/07/Hellfest-Open-Air-Festival.jpg' }} style={styles.imageBack}/>
-            <View>
-                {/* <Text h3>Sign In</Text> */}
-            </View>
         </View>
     );
 }
@@ -16,7 +12,7 @@ export const LoginScreen = (props) => {
 const styles = StyleSheet.create({
     imageBack: {
         width: '100%',
-        height: '70%'
+        height: '50%'
     },
     container: {
         flex: 1,
