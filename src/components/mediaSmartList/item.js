f128412a86773f6cd64f8bb82e5b2ead8aa2f16f
Dzmitry  Sakalouski <dzmitrysakalouski@MacBook-Pro-Dzmitry.local>
2019-12-07 02:22:52 +0300

    add installation on android

--- src/components/mediaSmartList/item.js
+++ src/components/mediaSmartList/item.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, PermissionsAndroid } from 'react-native';
 import { Button, Divider } from 'react-native-elements';
 import firebase from 'react-native-firebase';
+import RNFetchBlob from 'react-native-fetch-blob';
 
 const styles = StyleSheet.create({
     container: {
@@ -23,21 +24,36 @@ const styles = StyleSheet.create({
 
 export const MediaItem = props => { // todo
     const { name, ref } = props.item;
-    const [blob, setBlob] = useState('')
 
     const handleDownloadFile = () => {
-        const storage = firebase.storage();
+        const storage = firebase.storage().refFromURL('gs://mob-dby.appspot.com');
         storage.child(ref).getDownloadURL().then(url => {
-            var xhr = new XMLHttpRequest();
-            xhr.responseType = 'blob';
-            xhr.onload = function (event) {
-                var blob = xhr.response;
-                const blobUrl = URL.createObjectURL(blob);
-                setBlob(blobUrl);
-
-            };
-            xhr.open('GET', url);
-            xhr.send();
+            const uStr = storage.child(ref).path.split('/');
+            
+            PermissionsAndroid.requestMultiple([
+                PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+                PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+            ], {
+                title: 'Permission',
+                message: 'We need your permission.',
+            }).then(permRes => {
+                if (permRes['android.permission.READ_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED &&
+                    permRes['android.permission.WRITE_EXTERNAL_STORAGE'] === PermissionsAndroid.RESULTS.GRANTED) {
+                        RNFetchBlob
+                            .config({
+                                fileCache : true,
+                                addAndroidDownloads: {
+                                    useDownloadManager: true,
+                                    path : RNFetchBlob.fs.dirs.DownloadDir + '/' + uStr[uStr.length - 1],
+                                    notification: true
+                                }
+                            })
+                            .fetch('GET', url, {})
+                            .then((res) => {
+                                console.log('The file saved to ', res.path())
+                            });
+                }
+            })
         }).catch(err => console.log(err));
     };
 
