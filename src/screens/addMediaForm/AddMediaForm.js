f95d74ad6c944d8dcc6a423b919b4710e0004334
Dzmitry <Sakalouski>
2019-12-10 18:00:01 +0300

    add picker

--- src/screens/addMediaForm/AddMediaForm.js
+++ src/screens/addMediaForm/AddMediaForm.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Picker } from 'react-native';
+import { View, StyleSheet, Picker, Platform } from 'react-native';
 import { Text, Icon, Button } from 'react-native-elements';
+import FilePickerManager from 'react-native-file-picker';
+import DocumentPicker from 'react-native-document-picker';
 
 const styles = StyleSheet.create({
     optionsBtn: {
@@ -15,24 +17,44 @@ const styles = StyleSheet.create({
 
 export const AddMediaForm = props => {
     const [selectedType, setSelectedType] = useState('');
+    const [file, setFile] = useState(null);
+
+    const pickAndroid = () => {
+        FilePickerManager.showFilePicker(null, (response) => {
+            console.log('Response = ', response);
+
+            if (response.didCancel) {
+                console.log('User cancelled file picker');
+            }
+            else if (response.error) {
+                console.log('FilePickerManager Error: ', response.error);
+            }
+            else {
+                setFile(response);
+            }
+        });
+    }
+
+    const pickIOS = () => {
+        DocumentPicker.pick({
+            type: [DocumentPicker.types.allFiles],
+        })
+        .then(res => console.log(res))
+        .catch((err) => {
+            if (DocumentPicker.isCancel(err)) {
+              // User cancelled the picker, exit any dialogs or menus and move on
+            } else {
+              console.log('Cancel')
+            }
+        });
+    }
 
     const handlePickFile = () => {
-        // RNFileSelector.Show({
-        //     title: 'Select',
-        //     onDone: res => console.log(res),
-        //     onCancel: () => console.log('lklk')
-        // })
-        // RNFileSelector.Show(
-        //     {
-        //         title: 'Select File',
-        //         onDone: (path) => {
-        //             console.log('file selected: ' + path)
-        //         },
-        //         onCancel: () => {
-        //             console.log('cancelled')
-        //         }
-        //     }
-        // )
+        Platform.OS === 'android' ? pickAndroid() : pickIOS();
+    }
+
+    const handleUpload = () => {
+
     }
 
     return (
@@ -47,6 +69,14 @@ export const AddMediaForm = props => {
                 <Picker.Item label="Image" value={3} />
             </Picker>
             <Button onPress={handlePickFile} buttonStyle={styles.optionsBtn} title="Добавть файл" />
+            {
+                file && 
+                <View>
+                    <Text>File name: {file.fileName}</Text>
+                    <Text>File size: {file.size}</Text>
+                    <Button onPress={handleUpload} buttonStyle={styles.optionsBtn} title="Загрузить" />
+                </View>
+            }
         </View>
     );
 }
